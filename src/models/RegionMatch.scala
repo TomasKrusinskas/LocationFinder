e18fa2132fdccda407471dd34f65f21788cd7bf4
Tomas Krusinskas <tomaskrusinskas1@gmail.com>
2025-07-29 15:53:16 +0300

    Change Naming

--- src/models/RegionMatch.scala
+++ src/models/RegionMatch.scala
@@ -3,9 +3,7 @@ package models
 import upickle.default.*
 import upickle.implicits.key
 
-case class RegionMatch(
-  region: String,
-  @key("matched_locations") matchedLocations: List[String]
-)
+case class RegionMatch(region: String, matched_locations: List[String])
 object RegionMatch:
   implicit val rw: ReadWriter[RegionMatch] = macroRW
+
